5532b4fc275b3a03fb27ae026468e3d94256d254
Austin <austin.dober@gmail.com>
2019-07-14 19:04:22 -0400

    updated colors and fixed javascript loop

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,70 @@
   var letters = [
-      "a", "b",  "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
-  ];
+    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
   // array of words
   var play = [
-    "touchdown", "saftey", "punt", "kick return", "kick off", "penalty", "punt return", "field goal", "tackle", "broken tackle", "juke", "spin", "truck", "interception", "pass", "run", "pitch", "hurdle"
+    "touchdown", "saftey", "punt", "kick-return", "kick-off", "penalty", "punt-return", "field-goal", "tackle", "broken-tackle", "juke", "spin", "truck", "interception", "pass", "run", "pitch", "hurdle"
   ];
   //random word
   var randNum = Math.floor(Math.random() * play.length);
+  var chosenWord = play[randNum].toUpperCase();
+  var underscore = [];
+  //user Guesses
   var right = [];
   var wrong = [];
-  var chosenWord = play[randNum];
-  var underscore = [];
   //DOM manipulation
-  var hangWord = document.getElementsByClassName("underscore");
-  var wrongGuess= document.getElementsByClassName("wrongGuess")
-  var correctGuess = document.getElementsByClassName("correctGuess")
+  var gameBoard = document.getElementById("gameBoard")
+  var wrongGuess= document.getElementsByClassName("wrongGuess");
+  var restart=document.getElementById("restart");
+  var lives = document.getElementsByClassName("lives")
   //scoreboard
   var win = 0;
   var loses = 0;
   var lives = 10;
+ 
   //create underscore based on random word
   console.log(chosenWord)
-  var generateUnderscore = ()=>{
-    for(var i = 0; i <  chosenWord.length; i++){
-      underscore.push("_");
-    }
-    return underscore;
-  }
-
-  console.log(generateUnderscore());
-  //capture user guess
-    document.onkeypress = function(event){ 
-      var userGuess = String.fromCharCode(event.keyCode);
-      console.log (userGuess)
-      //User guess is right
-      if(chosenWord.indexOf(userGuess) > -1){
-        right.push(userGuess);
-        //swaps out underscore for user guess
-        underscore[chosenWord.indexOf(userGuess)] = userGuess;
-        //add guess to correct answer box
-        
-        correctGuess[0].append(userGuess)
-        //combines user guess to string
-        if(underscore.join(' ')===chosenWord){
-          alert("You Win!")
-          win++;
+    var generateUnderscore = ()=>{
+      for(var i = 0; i <  chosenWord.length; i++){
+        if(chosenWord[i]===("-")){
+          underscore.push(" - ")
+        }else{
+          underscore.push(" _ ");
         }
-      }else{
-        //move guess to incorrect box and remove live.
-        wrong.push(userGuess);
-        wrongGuess[0].append(userGuess)
-        lives--;
       }
-      
+      return underscore.join(" ");
+    }
+    //capture user guess
+    document.onkeypress = function(event){ 
+        var userGuess = String.fromCharCode(event.keyCode).toUpperCase();
+      //Validate user guess
+      if(letters.indexOf(userGuess)){
+        console.log("User Guessed: "+ userGuess);
+        //Loop to check guess vs. chosen word
+        for(var j = 0; j<chosenWord.length; j++){
+          if(chosenWord[j]!== userGuess){
+          wrong.push(userGuess)
+          console.log("these are wrong " + wrong)
+          lives--;
+          wrongGuess.innerHTML = wrong;
+          lives.innerHTML = lives;
+          }else{
+            underscore[j]=userGuess;
+            gameBoard.innerHTML= underscore.join(" ");
+          }
+        }
+      }
+      //Invalid Entry
+      else {
+        alert("Incorrect Entry please enter letter from A-Z")
+      };
     };
+    if(underscore.join(' ')===chosenWord){
+        alert("You Win")
+        win++;
+      };
+    
+    
+    
+// generate game board
+document.getElementById("gameBoard").innerHTML = generateUnderscore()
 
-  
