960ed4e02d1248a7dfbf5da28fd890f992228c12
Austin <austin.dober@gmail.com>
2019-07-18 15:11:35 -0400

    Submitting game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,71 +1,62 @@
-  var letters = [/a-z/];
-  // array of words
-  var animals = [
-    "dog", "cat", "parrot", "falcon", "eagle", "fish", "shark", "aligator", "turtle", "snake", "dolphin", "tiger", "lion", "horse", "zebra", "elephant", "mouse", "squirrle"
-  ];
-  //random word
-  var randNum = Math.floor(Math.random() * animals.length);
-  var chosenWord = animals[randNum];
-  var underscore = [];
-  //user Guesses
-  var right = [];
-  var wrong = [];
-  //DOM manipulation
-  var gameBoard = document.getElementById("gameBoard")
-  var wrongGuess= document.getElementsByClassName("wrongGuess");
-  var restart=document.getElementById("restart");
-  var lives = document.getElementsByClassName("lives")
-  //scoreboard
-  var win = 0;
-  var loses = 0;
-  var lives = 10;
-  var lettersLeft=underscore.length
-  //create underscore based on random word
-  console.log(chosenWord)
-    var generateUnderscore = ()=>{
-      for(var i = 0; i <  chosenWord.length; i++){
-        if(chosenWord[i]===("-")){
-          underscore.push(" - ")
-        }else{
-          underscore.push(" _ ");
-        }
-      }
-      return underscore.join(" ");
-    }
+// var letters = [/a-z/i];
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+// array of words
+var animals = [
+  "dog", "cat", "parrot", "falcon", "eagle", "fish", "shark", "aligator", "turtle", "snake", "dolphin", "tiger", "lion", "horse", "zebra", "elephant", "mouse", "squirrle"
+];
+//random word
+var chosenWord = animals[Math.floor(Math.random() * animals.length)];
+var underscore = [];
+//user Guesses
+var right = [];
+var wrong = [];
+// var userGuess=[];
+//DOM manipulation
+var gameBoard = document.getElementById("gameBoard")
+var wrongGuess= document.getElementsByClassName("wrongGuess");
+var restart=document.getElementById("restart");
+var live = document.getElementsByClassName("lives")
+//scoreboard
+var win = 0;
+var loses = 0;
+var guess = 10;
+var lettersLeft=chosenWord.length
+//create underscore based on random word
+console.log(chosenWord)
+
+for(var i = 0; i <  chosenWord.length; i++){
+      underscore[i]="  __  ";
+  }
+console.log(underscore)
+// generate game board
+gameBoard.innerHTML = underscore.join("  ")
+live.innerHTML=guess;
     //capture user guess
     document.onkeypress = function(event){ 
-        var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+      var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
       //Validate user guess
-      if(letters.indexOf(userGuess)){
-        console.log("User Guessed: "+ userGuess);
-        //Loop to check guess vs. chosen word
-        for(var j = 0; j<chosenWord.length; j++){
-          if(chosenWord[j]!== userGuess){
-            wrong.push(userGuess)
-            console.log("these are wrong " + wrong)
-            lives--;
-
-          }else{
-              underscore[j]== userGuess;
-              gameBoard.innerHTML= underscore.join(" ");
-              lettersLeft --;
-              if(lettersLeft===0){
-                alert("You Win")
-                win++;
+      if(letters.indexOf(userGuess)>-1){
+          if(chosenWord.indexOf(userGuess)>-1){
+          underscore[chosenWord.indexOf(userGuess)]=userGuess;
+          console.log(underscore)
+          gameBoard.innerHTML=underscore.join(" ")
+          }
+          if(underscore.join(" ")===chosenWord){
+            alert("you win")
+            win ++;
             }
-          } 
-          
-        }
-      }
-      //Invalid Entry
-      else {
-        alert("Incorrect Entry please enter letter from A-Z")
-      };
-    };
-   
+          else{
+              wrong.push(userGuess)
+              // console.log(wrong)
+              wrongGuess.innerHTML = wrong.join()
+          }
+      } 
+    }
+  
+
+  
     
-    
-    
-// generate game board
-document.getElementById("gameBoard").innerHTML = generateUnderscore()
+  
+  
+  
 
