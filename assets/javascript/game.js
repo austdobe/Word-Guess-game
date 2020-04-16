05c4580be3efd1285f7371f2fc1190930a742941
Austin <austin.dober@gmail.com>
2020-04-16 13:02:04 -0400

    updating HTML and working on game functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,62 +1,153 @@
 // var letters = [/a-z/i];
-var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 // array of words
 var animals = [
   "dog", "cat", "parrot", "falcon", "eagle", "fish", "shark", "aligator", "turtle", "snake", "dolphin", "tiger", "lion", "horse", "zebra", "elephant", "mouse", "squirrle"
 ];
 //random word
-var chosenWord = animals[Math.floor(Math.random() * animals.length)];
+var chosenWord = "";
 var underscore = [];
+var blanks = 0;
+var chosenLetters = [];
 //user Guesses
-var right = [];
-var wrong = [];
-// var userGuess=[];
-//DOM manipulation
-var gameBoard = document.getElementById("gameBoard")
-var wrongGuess= document.getElementsByClassName("wrongGuess");
-var restart=document.getElementById("restart");
-var live = document.getElementsByClassName("lives")
+var wrongLetters = [];
+
 //scoreboard
 var win = 0;
-var loses = 0;
-var guess = 10;
-var lettersLeft=chosenWord.length
-//create underscore based on random word
-console.log(chosenWord)
+var losses = 0;
+var guess = 9;
 
-for(var i = 0; i <  chosenWord.length; i++){
-      underscore[i]="  __  ";
-  }
-console.log(underscore)
-// generate game board
-gameBoard.innerHTML = underscore.join("  ")
-live.innerHTML=guess;
-    //capture user guess
-    document.onkeypress = function(event){ 
-      var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
-      //Validate user guess
-      if(letters.indexOf(userGuess)>-1){
-          if(chosenWord.indexOf(userGuess)>-1){
-          underscore[chosenWord.indexOf(userGuess)]=userGuess;
-          console.log(underscore)
-          gameBoard.innerHTML=underscore.join(" ")
-          }
-          if(underscore.join(" ")===chosenWord){
-            alert("you win")
-            win ++;
-            }
-          else{
-              wrong.push(userGuess)
-              // console.log(wrong)
-              wrongGuess.innerHTML = wrong.join()
-          }
-      } 
-    }
-  
+function startGame(){
 
-  
+  guess = 9;
+
+  chosenWord = animals[Math.floor(Math.random() * animals.length)];
+
+  chosenLetters = chosenWord.split("");
+  console.log(chosenLetters);
+  blanks = chosenLetters.length;
+
+  console.log(chosenWord);
+
+  underscore = [];
+
+  wrongGuess = [];
+
+  for (var i = 0; i < blanks; i++) {
+    underscore.push("_");
     
-  
-  
-  
+  }
+
+  console.log(underscore);
+
+  document.getElementById("lives").innerHTML = guess;
+
+  document.getElementById("underscore").innerHTML = underscore.join(" ");
+
+  document.getElementById("wrongGuess").innerHTML = wrongLetters.join(" ");
+}
+
+function checkLetters(letter){
+
+  var letterInWord = false;
+
+  for (var i = 0; i < blanks; i++) {
+    if (chosenWord[i] === letter)
+
+    letterInWord = true;
+    
+  }
+
+  if(letterInWord){
+    for (var j = 0; j < blanks; j++) {
+      if(chosenWord[j]===letter){
+        underscore[j] = letter;
+      }
+    }
+    console.log(underscore);
+  }else{
+    wrongGuesses.push(letter);
+
+    guess --;
+  }
+}
+
+function endRound(){
+  console.log("Win Counter: "+win + " | Loss counter: "+ loses + " | Guesses: "+ guess);
+
+
+  document.getElementById("lives").innerHTML = guess;
+
+  document.getElementById("gameBoard").innerHTML = underscore.join(" ");
+
+  document.getElementById("wrongGuess").innerHTML = wrongLetters.join(" ");
+
+  if(chosenLetters.toString()=== underscore.toString()){
+    win++;
+
+    alert("You Win!!!");
+
+    document.getElementById("wins").innerHTML = wins;
+
+    startGame();
+  } else if(guess === 0){
+    losses++;
+    alert("You lose");
+
+    document.getElementById("losses").innerHTML = losses;
+
+    startGame();
+  }
+}
+
+startGame();
+
+document.onkeyup = function(event){
+
+  userGuess = String.fromCharCode(event.which).toLowerCase();
+
+  checkLetters(userGuess);
+
+  endRound();
+};
+
+
+
+// function GenerateUnderScore(){
+// for(var i = 0; i <  chosenWord.length; i++){
+//       underscore[i]="  __  ";
+//   }
+// console.log(underscore)
+// // generate game board
+// gameBoard.innerHTML = underscore.join("  ")
+// }
+// GenerateUnderScore();
+// document.getElementsByClassName("live").innerHTML=guess;
+//     //capture user guess
+// document.onkeypress = function validateGuess(event){ 
+//   userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+//   //Validate user guess
+//   if(letters.indexOf(userGuess)>-1){
+//       checkGuess();
+//       checkScore();
+//       }
+//   else{
+//       wrong.push(userGuess);
+//       console.log(wrong);
+//       document.getElementsByClassName("wrongGuess").innerHTML = wrong.join();
+//       }
+//   };
+//   function checkGuess(){
+//     for(var j=i; j<chosenWord.length; j++){
+//       underscore[j]=userGuess;
+    
+//   }
+//   }
+//   function checkScore(){
+//     if(underscore===chosenWord){
+//       alert("you win")
+//       win ++;
+//         }
+//   }
+
 
